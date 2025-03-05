a7c93fe834727be9e066233e846cae33ca89088d
ahkjeflhkeuvha <w2317@e-mirim.hs.kr>
2025-03-05 09:32:07 +0900

    타입 예제

---
+++ add.js
@@ -0,0 +1,5 @@
+function AddTS(x, y) {
+    return x + y;
+}
+console.log(AddTS(10, 20));
+console.log(AddTS("10", "20"));
---
+++ add.ts
@@ -0,0 +1,6 @@
+function AddTS(x: number, y: number): number {
+  return x + y;
+}
+
+console.log(AddTS(10, 20));
+console.log(AddTS("10", "20"));
